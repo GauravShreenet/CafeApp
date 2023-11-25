6599c82d8bdfdf3587db02ad9059d465a867f5f2
Gaurav Shreenet <gthakuri656@gmail.com>
2023-11-25 23:41:00 +1100

    added images and navbar

--- src/Component/Menu.js
+++ src/Component/Menu.js
@@ -1,33 +1,26 @@
 import React from 'react'
 import firstLogo from '../Asset/Logo.png'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Button, Image } from 'react-bootstrap';
 
 export const Menu = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
+    <Navbar collapseOnSelect expand="lg" bg='dark' data-bs-theme="dark" className='fixed-top'>
+      <Container fluid className='mx-5'>
+        <Navbar.Brand href=""><Image src={firstLogo} alt=""/></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto gap-4">
+            <Nav.Link href="" className='text-light'>Home</Nav.Link>
+            <Nav.Link href="" className='text-light'>Menus</Nav.Link>
+            <Nav.Link href="" className='text-light'>Our Story</Nav.Link>
+            <Nav.Link href="" className='text-light'>Find Us</Nav.Link>
+            <Nav.Link href="" className='text-light'>Special Events</Nav.Link>
+            <Button variant='light' className='rounded-5 px-4'>
+                Order
+            </Button>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import backGround from '../Asset/cafeback.png'
+import { Container, Image } from 'react-bootstrap'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <>
+            <div>
+                <Image src={backGround} alt="" fluid/>
+            </div>
+        </>
+
+    )
 }
 
 export default Home
